aa46b52beabf1ba6a7762f90ce05dbdc01bac7a8
Kabanyok, Artyom <a.kabanyok@itransition.com>
2024-05-15 17:51:47 +0300

    Implemented conversion from int to Roman
    * Implement method intToRoman
    * Add tests

--- src/main/java/academy/kata/Converter.java
+++ src/main/java/academy/kata/Converter.java
@@ -1,9 +1,11 @@
 package academy.kata;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class Converter {
-    static final Map<String, Integer> mapRomanToArabic = Map.of(
+    private static final Map<String, Integer> mapRomanToInt = Map.of(
             "I", 1,
             "II", 2,
             "III", 3,
@@ -16,7 +18,54 @@ public class Converter {
             "X", 10
     );
 
-    static public Integer romanToArabic(String romanNumber){
-        return mapRomanToArabic.get(romanNumber);
+    private static final Map<Integer, String> mapIntegerToRoman = new HashMap<>() {{
+        put(1, "I");
+        put(2, "II");
+        put(3, "III");
+        put(4, "IV");
+        put(5, "V");
+        put(6, "VI");
+        put(7, "VII");
+        put(8, "VIII");
+        put(9, "IX");
+        put(10, "X");
+        put(40, "XL");
+        put(50, "L");
+        put(90, "XC");
+        put(100, "C");
+    }};
+
+    static public Integer romanToInteger(String romanNumber) {
+        return mapRomanToInt.get(romanNumber);
+    }
+
+    static public String intToRoman(int num) {
+        StringBuilder stringBuilder = new StringBuilder();
+        if (num < 1 || num > 100) {
+            return null;
+        }
+        if (num < 40) {
+            String romanTen = getRomanNumber(10);
+            stringBuilder.append(romanTen.repeat(num / 10));
+            stringBuilder.append(getRomanNumber(num % 10));
+        } else if (num < 50) {
+            stringBuilder.append(getRomanNumber(40));
+            stringBuilder.append(getRomanNumber(num % 40));
+        } else if (num == 50) {
+            return getRomanNumber(50);
+        } else if (num < 90) {
+            stringBuilder.append(getRomanNumber(50));
+            stringBuilder.append(intToRoman(num % 50));
+        } else if (num < 100) {
+            stringBuilder.append(getRomanNumber(90));
+            stringBuilder.append(getRomanNumber(num % 90));
+        } else if (num == 100) {
+            return getRomanNumber(100);
+        }
+        return stringBuilder.toString();
+    }
+
+    private static String getRomanNumber(int num) {
+        return Objects.toString(mapIntegerToRoman.get(num), "");
     }
 }
--- src/test/java/TestConverter.java
+++ src/test/java/TestConverter.java
@@ -7,27 +7,60 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 public class TestConverter {
 
     @Test
-    public void shouldBeCorrectConvert(){
-        assertEquals(1, Converter.romanToArabic("I"));
-        assertEquals(2, Converter.romanToArabic("II"));
-        assertEquals(3, Converter.romanToArabic("III"));
-        assertEquals(4, Converter.romanToArabic("IV"));
-        assertEquals(5, Converter.romanToArabic("V"));
-        assertEquals(6, Converter.romanToArabic("VI"));
-        assertEquals(7, Converter.romanToArabic("VII"));
-        assertEquals(8, Converter.romanToArabic("VIII"));
-        assertEquals(9, Converter.romanToArabic("IX"));
-        assertEquals(10, Converter.romanToArabic("X"));
+    public void shouldBeCorrectConvertRomanToInteger(){
+        assertEquals(1, Converter.romanToInteger("I"));
+        assertEquals(2, Converter.romanToInteger("II"));
+        assertEquals(3, Converter.romanToInteger("III"));
+        assertEquals(4, Converter.romanToInteger("IV"));
+        assertEquals(5, Converter.romanToInteger("V"));
+        assertEquals(6, Converter.romanToInteger("VI"));
+        assertEquals(7, Converter.romanToInteger("VII"));
+        assertEquals(8, Converter.romanToInteger("VIII"));
+        assertEquals(9, Converter.romanToInteger("IX"));
+        assertEquals(10, Converter.romanToInteger("X"));
     }
 
     @Test
-    public void shouldBeReturnNull(){
-        assertNull(Converter.romanToArabic("IIII"));
-        assertNull(Converter.romanToArabic("IIV"));
-        assertNull(Converter.romanToArabic("VIIII"));
-        assertNull(Converter.romanToArabic("IIX"));
-        assertNull(Converter.romanToArabic("XV"));
-        assertNull(Converter.romanToArabic("IVX"));
-        assertNull(Converter.romanToArabic("XI"));
+    public void shouldBeReturnNullIfIncorrectRomanNumberOrHigherTenWhenConvertRomanToInteger(){
+        assertNull(Converter.romanToInteger("IIII"));
+        assertNull(Converter.romanToInteger("IIV"));
+        assertNull(Converter.romanToInteger("VIIII"));
+        assertNull(Converter.romanToInteger("IIX"));
+        assertNull(Converter.romanToInteger("XV"));
+        assertNull(Converter.romanToInteger("IVX"));
+        assertNull(Converter.romanToInteger("XI"));
+    }
+
+    @Test
+    public void shouldBeCorrectConvertIntToRoman(){
+        assertEquals("I", Converter.intToRoman(1));
+        assertEquals("II", Converter.intToRoman(2));
+        assertEquals("III", Converter.intToRoman(3));
+        assertEquals("IV", Converter.intToRoman(4));
+        assertEquals("V", Converter.intToRoman(5));
+        assertEquals("VI", Converter.intToRoman(6));
+        assertEquals("VII", Converter.intToRoman(7));
+        assertEquals("VIII", Converter.intToRoman(8));
+        assertEquals("IX", Converter.intToRoman(9));
+        assertEquals("X", Converter.intToRoman(10));
+        assertEquals("XIX", Converter.intToRoman(19));
+        assertEquals("XXXIX", Converter.intToRoman(39));
+        assertEquals("XL", Converter.intToRoman(40));
+        assertEquals("L", Converter.intToRoman(50));
+        assertEquals("LI", Converter.intToRoman(51));
+        assertEquals("LXXX", Converter.intToRoman(80));
+        assertEquals("LXXXI", Converter.intToRoman(81));
+        assertEquals("LXXXIX", Converter.intToRoman(89));
+        assertEquals("XC", Converter.intToRoman(90));
+        assertEquals("XCI", Converter.intToRoman(91));
+        assertEquals("XCIX", Converter.intToRoman(99));
+        assertEquals("C", Converter.intToRoman(100));
+    }
+
+    @Test
+    public void shouldBeReturnNullIfNumberIsHigherHundredOrLessOneWhenConvertIntToRoman(){
+        assertNull(Converter.intToRoman(-1));
+        assertNull(Converter.intToRoman(0));
+        assertNull(Converter.intToRoman(101));
     }
 }
